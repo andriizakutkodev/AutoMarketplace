d5478b9a2d2996fd389741de9522d87af050cf0e
Andrii Zakutko <zakutkoandrey@gmail.com>
2024-12-01 13:35:52 +0100

    added the PostStatus enum

---
+++ Domain/Enums/PostStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Enums;
+
+/// <summary>
+/// Represents the post statuses enum.
+/// </summary>
+public enum PostStatus
+{
+    /// <summary>
+    /// Pending status.
+    /// </summary>
+    Pending = 0,
+
+    /// <summary>
+    /// Published status.
+    /// </summary>
+    Published,
+
+    /// <summary>
+    /// Canceled status.
+    /// </summary>
+    Canceled,
+}
