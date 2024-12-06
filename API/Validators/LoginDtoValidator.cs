78d27a06467393dafd582dc78d1f19dd9c5cfb89
Andrii Zakutko <andrii.zakutko@nixs.com>
2024-12-06 17:34:19 +0100

    The LoginDtoValidator and RegisterDtoValidator were added to API project

---
+++ API/Validators/LoginDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿namespace API.Validators;
+
+using Application.DTOs;
+using FluentValidation;
+
+/// <summary>
+/// Validator for the <see cref="LoginDto"/> class.
+/// Ensures that the email and password properties meet the required validation criteria.
+/// </summary>
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoginDtoValidator"/> class.
+    /// Defines validation rules for the <see cref="LoginDto"/> object.
+    /// </summary>
+    public LoginDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"\d").WithMessage("Password must contain at least one digit.")
+            .Matches(@"[\W_]").WithMessage("Password must contain at least one special character.");
+    }
+}
---
+++ API/Validators/RegisterDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿namespace API.Validators;
+
+using Application.DTOs;
+using FluentValidation;
+
+/// <summary>
+/// Validator for the <see cref="RegisterDto"/> class.
+/// Ensures that the email, name, surname, phone number, and password properties meet the required validation criteria.
+/// </summary>
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RegisterDtoValidator"/> class.
+    /// Defines validation rules for the <see cref="RegisterDto"/> object.
+    /// </summary>
+    public RegisterDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.");
+        RuleFor(x => x.Surname)
+            .NotEmpty().WithMessage("Surname is required.");
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("PhoneNumber is required.");
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"\d").WithMessage("Password must contain at least one digit.")
+            .Matches(@"[\W_]").WithMessage("Password must contain at least one special character.");
+    }
+}
