2c2ceb0196319a7e8de9dc4f1723d68ecd9b14ab
Andrii Zakutko <zakutkoandrey@gmail.com>
2024-12-07 09:00:34 +0100

    Reformat the load dependecies logic

--- API/Dependencies/Dependencies.cs
+++ API/Dependencies/Dependencies.cs
@@ -1,8 +1,11 @@
 ﻿namespace API.Dependencies;
 
+using API.Validators;
+using Application.DTOs;
 using Application.Interfaces;
 using Application.Services;
 using Domain.Entities;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Interfaces;
 using Persistence.Repositories;
@@ -14,16 +17,31 @@ using Persistence.Repositories;
 public static class Dependencies
 {
     /// <summary>
-    /// Registers application services and repositories into the provided <see cref="IServiceCollection"/> instance.
-    /// This includes business logic services and data access layer components.
+    /// Register all needed components to the IoC.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to register dependencies with.</param>
     public static void Load(IServiceCollection services)
     {
-        services.AddScoped<IPostsService, PostsService>();
+        RegisterServices(services);
+        RegisterRepositories(services);
+        RegisterValidators(services);
+    }
 
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.AddScoped<IPostsService, PostsService>();
+    }
+
+    private static void RegisterRepositories(IServiceCollection services)
+    {
         services.AddScoped<IGenericRepository<Post>, PostsRepository>();
         services.AddScoped<IGenericRepository<Vehicle>, VehiclesRepository>();
         services.AddScoped<IGenericRepository<User>, UsersRepository>();
+    }
+
+    private static void RegisterValidators(IServiceCollection services)
+    {
+        services.AddScoped<IValidator<LoginDto>, LoginDtoValidator>();
+        services.AddScoped<IValidator<RegisterDto>, RegisterDtoValidator>();
     }
 }
