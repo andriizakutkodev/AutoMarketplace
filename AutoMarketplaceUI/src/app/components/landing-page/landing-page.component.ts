a3d0dfa1b06d36d80603bd51744daf98112df78d
Serhii Zakutko <serhii.zakutko@nixsolutions.com>
2024-12-11 00:02:31 +0100

    added landing page

---
+++ AutoMarketplaceUI/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
