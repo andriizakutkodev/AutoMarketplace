35d0621fb6f74ac1afbad695d95e74535c342220
Serhii Zakutko <sergeyzakutko2000@gmail.com>
2024-12-11 15:19:39 +0100

    added header component

--- AutoMarketplaceUI/src/app/app.routes.ts
+++ AutoMarketplaceUI/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent }
 ];
